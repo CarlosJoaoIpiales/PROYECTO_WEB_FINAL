127952f0cc2d88d90349437758c3abfd330c3b93
Shadow_Moon <cjipiales1@espe.edu.ec>
2023-08-08 20:48:22 -0500

    Se mejoro la funcion de php para el login separando los if's para que no se muestren alerts al presionar el boton atras

--- index.php
+++ index.php
@@ -5,35 +5,91 @@ include('./php/conectionDB.php');
 
 //Enter or press button ingresar
 if (isset($_POST['submit'])) {
+    /*
     //getting the post values
     $emailEnter = $_POST['email'];
-    $userEnter = $_POST['email'];
     $passwordEnter = $_POST['password'];
 
     //search dates in DB
-    $ret = mysqli_query($con, "select * from usuarios");
+    $ret = mysqli_query($con, "select * from usuarios where u_correo = '$emailEnter' or u_nick = '$emailEnter'");
     $row = mysqli_num_rows($ret);
     if ($row > 0) {
+        //Search users in table usuarios
         while ($row = mysqli_fetch_array($ret)) {
             $user = $row['u_nick'];
             $email = $row['u_correo'];
             $password = $row['u_contraseña'];
             $idUser = $row['idUsuario'];
-            if ($user == $userEnter || $email == $emailEnter) {
+            if ($user == $emailEnter || $email == $emailEnter) {
                 if ($password == $passwordEnter) {
                     $rol_user = mysqli_query($con, "select * from roles_usuarios");
                     $row1 = mysqli_num_rows($rol_user);
+                    //Search in table roles_usuarios
                     while ($row1 = mysqli_fetch_array($rol_user)) {
-                        $idUser1 = $row['idUsuario'];
-                        $idRol = $row['idUsuario'];
+                        $idUser1 = $row1['idUsuario'];
+                        $idRol = $row1['idRol'];
                         if ($idUser1 == $idUser && $idRol == 1) {
                             header("Location: php/adminView.php");
                             exit;
                         }
                     }
+                }else {
+                    echo "<script>alert('La contraseña es incorrecta');</script>";
                 }
+            }else {
+                echo "<script>alert('El correo o el usuario son incorrectos');</script>";
             }
         }
+    }
+}
+    
+    */
+
+    // Inicia la sesión
+    session_start();
+
+    //getting the post values
+    $emailEnter = $_POST['email'];
+    $passwordEnter = $_POST['password'];
+
+    //search dates in DB
+    $ret = mysqli_query($con, "select * from usuarios");
+    $row = mysqli_num_rows($ret);
+    $authenticated = false;
+
+    // Search users in table usuarios
+    while ($row = mysqli_fetch_array($ret)) {
+        $user = $row['u_nick'];
+        $email = $row['u_correo'];
+        $password = $row['u_contraseña'];
+        $idUser = $row['idUsuario'];
+
+        if ($user == $emailEnter || $email == $emailEnter) {
+            if ($password == $passwordEnter) {
+                $authenticated = true;
+                break;
+            } else {
+                echo "<script>alert('La contraseña es incorrecta');</script>";
+            }
+        }
+    }
+
+    if ($authenticated) {
+        $rol_user = mysqli_query($con, "select * from roles_usuarios");
+        $row1 = mysqli_num_rows($rol_user);
+
+        // Search in table roles_usuarios
+        while ($row1 = mysqli_fetch_array($rol_user)) {
+            $idUser1 = $row1['idUsuario'];
+            $idRol = $row1['idRol'];
+
+            if ($idUser1 == $idUser && $idRol == 1) {
+                header("Location: php/adminView.php");
+                exit;
+            }
+        }
+    } else {
+        echo "<script>alert('El correo o el usuario son incorrectos');</script>";
     }
 }
 
--- php/adminView.php
+++ php/adminView.php
@@ -1,11 +1,20 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Se ingreso a otra pagina</h1>
+    <h1>Bienvenido</h1>
+    <p>Estás conectado como Usuario ID: 1</p>
+
+    <a href="loginOut.php">Cerrar Sesión</a>
 </body>
+
 </html>
